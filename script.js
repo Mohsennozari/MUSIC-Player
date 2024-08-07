a9546fa23cbaa196cd4ab836b9c8b66019d4a737
mohsen <mohsennozari13822003@gmail.com>
2024-08-07 16:47:20 +0330

    final

--- script.js
+++ script.js
@@ -2,13 +2,35 @@ let progress = document.getElementById("progress");
 let song = document.getElementById("song");
 let ctrlIcon = document.getElementById("ctrlIcon");
 
-song.onloadmetadata = function(){
- progress.max = song.duration;
-  progress.value = song.currentTime;
-   progress.max = song.duration;
+song.onloadedmetadata = function() {
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+};
+
+function playPause() {
+    if (ctrlIcon.classList.contains("fa-pause")) {
+        song.pause();
+        ctrlIcon.classList.remove("fa-pause");
+        ctrlIcon.classList.add("fa-play");
+    } else {
+        song.play();
+        ctrlIcon.classList.add("fa-pause");
+        ctrlIcon.classList.remove("fa-play");
+    }
 }
 
-function playpuss(){
+// بروزرسانی نوار پیشرفت
+setInterval(() => {
+    if (!song.paused) {
+        progress.value = song.currentTime;
+    }
+}, 500);
 
-
-}
+progress.oninput = function() {
+    song.currentTime = progress.value;
+    if (song.paused) {
+        song.play();
+        ctrlIcon.classList.add("fa-pause");
+        ctrlIcon.classList.remove("fa-play");
+    }
+};
